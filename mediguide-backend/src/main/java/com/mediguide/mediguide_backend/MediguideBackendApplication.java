56f47b81a28d18fd7c5b35c41f7b63fedb493a95
Vishnu-reddy01 <nagaralavishnuvardhanreddy@gmail.com>
2026-08-03 07:22:05 +0530

    Milestone 1 - Spring Boot and MySQL setup

---
+++ mediguide-backend/src/main/java/com/mediguide/mediguide_backend/MediguideBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mediguide.mediguide_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediguideBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediguideBackendApplication.class, args);
+	}
+
+}
---
+++ mediguide-backend/src/test/java/com/mediguide/mediguide_backend/MediguideBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediguide.mediguide_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediguideBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
